f93a7ba08a4c82441673c1a4781d6abe91eb5140
Sinan Tunç <sinantunc878@gmail.com>
2021-09-11 11:47:51 +0300

    complete Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,57 +1,10 @@
-import { useRouter } from "next/router";
-import Link from "next/link";
+import Navbar from "./Navbar";
 
-const Navbar = () => {
-  const router = useRouter();
-  const isActive = (r) => (r === router.pathname ? "active" : "");
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+);
 
-  return (
-    <nav
-      className="navbar navbar-expand-lg navbar-light"
-      style={{ backgroundColor: "#e3f2fd" }}
-    >
-      <Link href="/">
-        <a className="navbar-brand">
-          <i className="fas fa-store-alt mx-1" aria-hidden="true"></i>E-SHOP
-        </a>
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div
-        className="collapse navbar-collapse justify-content-end "
-        id="navbarNav"
-      >
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link href="/cart">
-              <a className={`nav-link ${isActive("/cart")}`}>
-                <i className="fas fa-shopping-bag mx-1" aria-hidden="true"></i>
-                Cart
-              </a>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link href="/login">
-              <a className={`nav-link ${isActive("/login")}`}>
-                <i className="fas fa-user-circle mx-1" aria-hidden="true"></i>
-                Login
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar;
+export default Layout;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,57 @@
-const Navbar = () => <nav>Navbar</nav>;
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const Navbar = () => {
+  const router = useRouter();
+  const isActive = (r) => (r === router.pathname ? "active" : "");
+
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light"
+      style={{ backgroundColor: "#e3f2fd" }}
+    >
+      <Link href="/">
+        <a className="navbar-brand">
+          <i className="fas fa-store-alt mx-1" aria-hidden="true"></i>E-SHOP
+        </a>
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div
+        className="collapse navbar-collapse justify-content-end "
+        id="navbarNav"
+      >
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link href="/cart">
+              <a className={`nav-link ${isActive("/cart")}`}>
+                <i className="fas fa-shopping-bag mx-1" aria-hidden="true"></i>
+                Cart
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/login">
+              <a className={`nav-link ${isActive("/login")}`}>
+                <i className="fas fa-user-circle mx-1" aria-hidden="true"></i>
+                Login
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
